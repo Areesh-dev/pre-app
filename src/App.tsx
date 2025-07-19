c041a8eb573f97d1097cc202a2c5a3c6daadcdee
Muhammad Habib <habibsahabubit@gmail.com>
2025-07-20 03:21:00 +0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-9xl'>Hello World!</h1>
+      <p className='text-5xl'>Muahammad Areesh</p>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+ content: ["./index.html","./src/**/*.{html,js,vue,ts,jsx,tsx,md,mdx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
